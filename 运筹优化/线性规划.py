cf0e3b125a7a1393784ab54855df657ddef8d674
陈雪泽 <116641047+AXiaoming666@users.noreply.github.com>
2024-08-02 22:25:20 +0800

    xiaoming

--- 运筹优化/线性规划.py
+++ 运筹优化/线性规划.py
@@ -15,28 +15,33 @@ b_lq = np.array([])    # 不等式约束条件的常数项向量(小于等于)
 A_eq = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])    # 等式约束条件的变量系数矩阵
 b_eq = np.array([0])    # 等式约束条件的常数项向量
 X = np.array([[0,None],[0,None],[0,None],[0,None],[0,None],[0,None],[0,None],[0,None],[0,None],[0,None],[0,None],[0,None],[0,None],[0,None],[0,None],[0,None]])    # 决策变量xi的取值范围矩阵(正无穷为None)
+Mxx = 0    # 最大化问题取1，最小化问题取0
 
 
-# 统一为小于等于不等式的形式
-if np.size(A_lq) & np.size(A_gq):
-        A_ub = np.concatenate((A_lq,-A_gq), axis=0)
-        b_ub = np.concatenate((b_lq,-b_gq), axis=0)
-else:
-    if np.size(A_lq) == 0:
-        A_ub = -A_gq
-        b_ub = -b_gq
+# 定义线性规划函数
+def LP(C, A_gq, b_gq, A_lq, b_lq, A_eq, b_eq, X, Mxx):
+    # 统一为小于等于不等式的形式
+    if np.size(A_lq) & np.size(A_gq):
+            A_ub = np.concatenate((A_lq,-A_gq), axis=0)
+            b_ub = np.concatenate((b_lq,-b_gq), axis=0)
     else:
-        A_ub = A_lq
-        b_ub = b_lq
+        if np.size(A_lq) == 0:
+            A_ub = -A_gq
+            b_ub = -b_gq
+        else:
+            A_ub = A_lq
+            b_ub = b_lq
 
 
-# 若为最大化问题，价值函数两边取负号，统一为最小化问题;若为最小化问题，直接代入linprog函数
-# 最大化问题取1，最小化问题取0
-if 0:   #最大化问题
-    result = linprog(-C, A_ub, b_ub, A_eq, b_eq, X)
-    print("x =", result.x)
-    print("F(x) =", -result.fun)
-else:   #最小化问题
-    result = linprog(C, A_ub, b_ub, A_eq, b_eq, X)
-    print("x =", result.x)
-    print("F(x) =", result.fun)
+    # 若为最大化问题，价值函数两边取负号，统一为最小化问题;若为最小化问题，直接代入linprog函数
+    if Mxx:    #最大化问题
+        result = linprog(-C, A_ub, b_ub, A_eq, b_eq, X)
+        result.fun = -result.fun
+    else:    #最小化问题
+        result = linprog(C, A_ub, b_ub, A_eq, b_eq, X)
+    return result
+
+
+result = LP(C, A_gq, b_gq, A_lq, b_lq, A_eq, b_eq, X, Mxx)
+print("x =", result.x)
+print("F(x) =", result.fun)
