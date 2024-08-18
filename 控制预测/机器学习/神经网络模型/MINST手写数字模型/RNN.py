a60af69e405e1b3c47d68681e7ad0c49ec34330a
陈雪泽 <116641047+AXiaoming666@users.noreply.github.com>
2024-08-18 12:17:18 +0800

    add CNN.py

--- 控制预测/机器学习/神经网络模型/MINST手写数字模型/RNN.py
+++ 控制预测/机器学习/神经网络模型/MINST手写数字模型/RNN.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.layers import Dense, SimpleRNN
 from tensorflow.keras.utils import to_categorical
 # 无视以上警告，能正常运行
 from Load_Data import X_train, y_train, X_test, y_test
@@ -8,23 +8,19 @@ from Load_Data import X_train, y_train, X_test, y_test
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data() """
 
 # 数据预处理
-X_train = X_train.reshape(-1, 28, 28, 1).astype('float32') / 255
-X_test = X_test.reshape(-1, 28, 28, 1).astype('float32') / 255
+X_train = X_train.reshape(-1, 28*28, 1).astype('float32') / 255
+X_test = X_test.reshape(-1, 28*28, 1).astype('float32') / 255
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
-# 建立CNN模型
+# 建立RNN模型
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
+model.add(SimpleRNN(64, input_shape=(28*28, 1)))  # 更新输入形状
+model.add(Dense(32, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
 # 编译模型
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # 训练模型
 model.fit(X_train, y_train, epochs=5, batch_size=128, validation_data=(X_test, y_test))
